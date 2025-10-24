745ef8ac8256eff82ff5bb50c73b014c5e6d0023
chandankumar.jha <chandankumar.jha@dish.com>
2025-10-24 14:28:26 +0530

    Q = ReverseArray P = 2 Pointers

---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        reverseArray(arr);
+    }
+
+    private static void reverseArray(int[] arr) {
+        int size = arr.length;
+        int start = 0;
+        int end = size - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            end--;
+            start++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
